6add0a19b8d7420f2418a6d85beb5706674f2d38
Ruben Dawagne <daw19003@byui.edu>
2022-03-09 21:24:03 -0700

    First commit with html and start scss

---
+++ credit/script.js
@@ -0,0 +1,7 @@
+function openForm() {
+  document.getElementById("ebranch-login-dropdown").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("ebranch-login-dropdown").style.display = "none";
+}
